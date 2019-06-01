a747edd23b4768814867b32c602efa99fdf319dd
Ray Wagner <raywagner88@me.com>
2019-06-01 00:06:57 -0500

    Move to postgresql and remove musicians

--- db/migrate/20171218221703_root_setup.rb
+++ db/migrate/20171218221703_root_setup.rb
@@ -1,18 +1,8 @@
 class RootSetup < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.timestamps
-      t.string :email
-    end
-
-    create_table :musicians do |t|
-      t.timestamps
-      t.string :name
-      t.integer :band
-    end
-
-    change_table :users do |t|
       ## Database authenticatable
+      t.string :email
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -28,6 +18,8 @@ class RootSetup < ActiveRecord::Migration[5.1]
       t.datetime :last_sign_in_at
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
+
+      t.timestamps
     end
 
     add_index :users, :email,                unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2017_12_18_221703) do
 
-  create_table "musicians", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.integer "band"
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "email"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -32,6 +26,8 @@ ActiveRecord::Schema.define(version: 2017_12_18_221703) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
