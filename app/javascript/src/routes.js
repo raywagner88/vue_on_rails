26066c1dbda21085ae8f9cfae160adaf4ae3b4a0
Ray Wagner <raywagner88@me.com>
2019-06-04 23:08:15 -0500

    Fix routes to home

--- app/javascript/src/routes.js
+++ app/javascript/src/routes.js
@@ -10,7 +10,7 @@ import Error404 from './components/errors/404.vue';
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/home', component: HomeIndex, name: 'root_path' },
+    { path: '/', component: HomeIndex, name: 'root_path' },
     { path: '/errors', component: ErrorsIndex, name: 'errors_path' },
     { path: '/500', component: Error500 },
     { path: '/404', component: Error404 },
