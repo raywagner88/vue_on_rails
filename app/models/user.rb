4176a98648e028465afbc421666e96b15747638e
Ray Wagner <raywagner88@me.com>
2019-06-11 22:03:11 -0500

    Format json date

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :trackable, :recoverable, :validatable
+
+  def as_json(options = {})
+    super.merge(created_at: created_at.strftime('%m/%d/%Y'))
+  end
 end
