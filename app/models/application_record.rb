1daaed2874ad1afafc4a84333541f5702cca0b5a
Ray Wagner <raywagner88@me.com>
2019-06-11 22:04:21 -0500

    Move date formatter to base

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def as_json(options = {})
+    super.merge(created_at: created_at.strftime('%m/%d/%Y'))
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :trackable, :recoverable, :validatable
-
-  def as_json(options = {})
-    super.merge(created_at: created_at.strftime('%m/%d/%Y'))
-  end
 end
